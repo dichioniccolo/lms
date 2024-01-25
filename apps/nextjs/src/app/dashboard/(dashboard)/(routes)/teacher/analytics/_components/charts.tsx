111b93c3a0c60303076069749ecd068203bfe5a2
Niccolò Di Chio <dichioniccolo@gmail.com>
2024-01-25 23:21:52 +0100

    fix: empty chart

--- apps/nextjs/src/app/dashboard/(dashboard)/(routes)/teacher/analytics/_components/charts.tsx
+++ apps/nextjs/src/app/dashboard/(dashboard)/(routes)/teacher/analytics/_components/charts.tsx
@@ -2,7 +2,7 @@
 
 import { Bar, BarChart, ResponsiveContainer, XAxis, YAxis } from "recharts";
 
-import { Card } from "@acme/ui/components/ui/card";
+import { Card, CardContent } from "@acme/ui/components/ui/card";
 
 interface Props {
   data: {
@@ -15,25 +15,33 @@ interface Props {
 export function Charts({ data }: Props) {
   return (
     <Card>
-      <ResponsiveContainer width="100%" height={350}>
-        <BarChart data={data}>
-          <XAxis
-            dataKey="title"
-            stroke="#888888"
-            fontSize={12}
-            tickLine={false}
-            axisLine={false}
-          />
-          <YAxis
-            stroke="#888888"
-            fontSize={12}
-            tickLine={false}
-            axisLine={false}
-            tickFormatter={(value) => `$${value}`}
-          />
-          <Bar dataKey="earnings" fill="#0369a1" radius={[4, 4, 0, 0]} />
-        </BarChart>
-      </ResponsiveContainer>
+      <CardContent>
+        {data.length === 0 ? (
+          <div className="flex h-full min-h-64 items-center justify-center">
+            <span>No data to show</span>
+          </div>
+        ) : (
+          <ResponsiveContainer width="100%" height={350}>
+            <BarChart data={data}>
+              <XAxis
+                dataKey="title"
+                stroke="#888888"
+                fontSize={12}
+                tickLine={false}
+                axisLine={false}
+              />
+              <YAxis
+                stroke="#888888"
+                fontSize={12}
+                tickLine={false}
+                axisLine={false}
+                tickFormatter={(value) => `$${value}`}
+              />
+              <Bar dataKey="earnings" fill="#0369a1" radius={[4, 4, 0, 0]} />
+            </BarChart>
+          </ResponsiveContainer>
+        )}
+      </CardContent>
     </Card>
   );
 }
