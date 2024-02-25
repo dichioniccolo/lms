68af9dd7904fc6c1210945b3073c42b9a30fe177
Niccolò Di Chio <dichioniccolo@gmail.com>
2024-02-25 20:28:37 +0100

    fix: remove welcome mail

--- packages/emails/index.ts
+++ packages/emails/index.ts
@@ -2,4 +2,3 @@ export * from "./emails";
 export { CourseInvitation } from "./emails/CourseInvitation";
 export { CoursePurchased } from "./emails/CoursePurchased";
 export { LoginLink } from "./emails/LoginLink";
-export { WelcomeEmail } from "./emails/WelcomeEmail";
