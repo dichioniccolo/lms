2964468617906002d580f29be0b719c31ed35321
Niccolo' Di Chio <dichioniccolo@gmail.com>
2025-01-21 18:06:27 +0100

    fix: revert to default error because we have sentry

--- apps/nextjs/src/app/global-error.tsx
+++ apps/nextjs/src/app/global-error.tsx
@@ -3,6 +3,7 @@
 // Error boundaries must be Client Components
 import { useEffect } from "react";
 import Error from "next/error";
+import NextError from "next/error";
 import * as Sentry from "@sentry/nextjs";
 
 export default function GlobalError({
@@ -20,8 +21,11 @@ export default function GlobalError({
     // global-error must include html and body tags
     <html>
       <body>
-        <h2>Qualcosa è andato storto!</h2>
-        <button onClick={() => reset()}>Riprova</button>
+        {/* `NextError` is the default Next.js error page component. Its type
+        definition requires a `statusCode` prop. However, since the App Router
+        does not expose status codes for errors, we simply pass 0 to render a
+        generic error message. */}
+        <NextError statusCode={0} />
       </body>
     </html>
   );
